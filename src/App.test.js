0999c22192412741fcd981df29d8975183f27cde
Brandon G-Home <brandon.gardner2@outlook.com>
2019-04-10 11:37:08 -0500

    tested RTL vs RTR snapshots

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import banana from 'react-test-renderer'; // 1: install this npm module as a dev dependency
+import React from "react";
+import { render } from "react-testing-library";
+import banana from "react-test-renderer"; // 1: install this npm module as a dev dependency
 
-import App from './App';
+import App from "./App";
 
-describe('<App />', () => {
+describe("<App />", () => {
   // 2. write this test
-  it('matches snapshot', () => {
+  it("matches snapshot", () => {
     const tree = banana.create(<App />); // generates a DOM tree
 
     // snapshots are a JSON representation of the DOM tree
     expect(tree.toJSON()).toMatchSnapshot();
   });
+
+  it("matches snapshot RTL", () => {
+    const { container } = render(<App />);
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
 });
+
+//testing both snapshots
